2c73fafaaf35e289de120202715dc1b10f5ecd0a
KMarcano <kelvin.marcano@md-digitalsolutions.com>
2023-02-04 11:29:37 -0400

    Ajustes en rol

--- resources/views/pages/ajustes/roles.blade.php
+++ resources/views/pages/ajustes/roles.blade.php
@@ -10,7 +10,7 @@
             </div>
             <div class="block-content">
                 <div class="col-sm-6 col-xl-4">
-                    <button type="button" class="btn btn-secondary">Nuevo</button>
+                    <button type="button" class="btn btn-secondary" onclick="rolModal();">Nuevo</button>
                 </div>
                 <div class="table-responsive">
                             <table class="table table-hover table-vcenter">
@@ -22,19 +22,20 @@
                                 <th class="text-center" style="width: 100px;">Accion</th>
                             </tr>
                             </thead>
+
                             <tbody>
-                            @foreach($rol as $rol)
+                                @foreach($roles as $rol)
                             <tr>
-                                <th class="text-center" scope="row">1{{$rol->id}}</th>
+                                <th class="text-center" scope="row">{{$rol->id}}</th>
                                 <td class="fw-semibold">
                                 <a href="be_pages_generic_profile.html">{{$rol->description}}</a>
                                 </td>
                                 <td class="fw-semibold">
-                                <a href="be_pages_generic_profile.html">{{$rol->type_rol}}</a>
+                                <a href="be_pages_generic_profile.html">admin</a>
                                 </td>
                                 <td class="text-center">
                                 <div class="btn-group">
-                                    <button type="button" class="btn btn-sm btn-alt-secondary" data-bs-toggle="tooltip" title="Ver">
+                                    <button type="button" class="btn btn-sm btn-alt-secondary" data-bs-toggle="tooltip" title="Ver" data-id='{{ $rol->id }}' onclick="verRolModal();">
                                     <i class="fa fa-eye"></i>
                                     </button>
                                 </div>
@@ -49,3 +50,77 @@
         </div>
     </div>
 @endsection
+
+  <!-- Modal New Rol-->
+  <div class="modal fade" id="rolModal" >
+    <div class="modal-dialog">
+    <!-- Modal content-->
+    <div class="modal-content">
+        <div class="modal-header">
+            <h4 class="modal-title">Agregar Nuevo Rol</h4>
+            <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+        </div>
+        <div class="modal-body">
+                <div class="col-lg-8 col-xl-5">
+                    <div class="mb-4">
+                      <label class="form-label" for="example-text-input">Descripcion</label>
+                      <input type="text" class="form-control" id="description" name="description" placeholder="Descripcion Rol">
+                    </div>
+                    <div class="mb-4">
+                      <label class="form-label" for="example-select">Tipo de Rol</label>
+                      <select class="form-select" id="example-select" name="example-select">
+                        <option selected>Seleccione:</option>
+                        <option value="1">Gerente</option>
+                        <option value="2">Coordinador</option>
+                        <option value="3">Comercial</option>
+                      </select>
+                    </div>
+                </div>
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+        </div>
+    </div>
+    </div>
+</div>
+
+<!-- Modal Ver Rol-->
+<div class="modal fade" id="VerRolModal" >
+    <div class="modal-dialog">
+    <!-- Modal content-->
+    <div class="modal-content">
+        <div class="modal-header">
+            <h4 class="modal-title">Ver Rol</h4>
+            <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+        </div>
+        <div class="modal-body">
+                <div class="col-lg-8 col-xl-5">
+                    <div class="mb-4">
+                        <label class="form-label" for="example-static-input-plain">Descripcion</label>
+                        <input type="text" readonly class="form-control-plaintext" id="description" name="description">
+                        </div>
+                    </div>
+                    <div class="mb-4">
+                        <label class="form-label" for="example-static-input-plain">Tipo de Rol</label>
+                        <input type="text" readonly class="form-control-plaintext" id="type_rol" name="type_rol">
+                    </div>
+                </div>
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+        </div>
+    </div>
+    </div>
+</div>
+<script type='text/javascript'>
+   function rolModal(){
+        $('#rolModal').modal('show'); 
+   }
+
+   function verRolModal(){
+        var rolId = $(this).attr('data-id');
+        alert(rolId);
+        $('#VerRolModal').modal('show'); 
+   }
+
+</script>
