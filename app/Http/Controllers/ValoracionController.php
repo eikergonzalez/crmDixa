b6f733ecdc4e134c78ed689bd9e88d5836fec478
Eiker <eiker.gonzalez21>
2023-03-08 12:27:28 -0400

    ajustes valoracion

--- app/Http/Controllers/ValoracionController.php
+++ app/Http/Controllers/ValoracionController.php
@@ -12,13 +12,15 @@ use App\Models\tipo_inmueble;
 use App\Models\User;
 use App\services\Response;
 use App\services\AgendaService;
+use App\services\FilesService;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
 
-class ValoracionController extends Controller
-{
+class ValoracionController extends Controller{
+
     public function index(){
         $data = []; 
 
@@ -88,60 +90,106 @@ class ValoracionController extends Controller
     }
 
     public function getArchivos(Request $request, $id){
-        $data['files'] = (new relacion_inmueble_archivos())
+        $data = (new relacion_inmueble_archivos())
             ->selectRaw("relacion_inmueble_archivos.*, tipo_archivo.descripcion as tipoDescri")
             ->where('inmueble_id', $id)
-            ->join('tipo_archivo','tipo_archivo.id','=','relacion_inmueble_archivos.tipo_archivo')
+            ->leftjoin('tipo_archivo','tipo_archivo.id','=','relacion_inmueble_archivos.tipo_archivo')
             ->get();
-        return Response::statusJson($request, "success", 'success', 'getArchivos', $data);
+        return Response::statusJson("success", 'success', 'getArchivos', $data);
     }
 
-    public function saveArchivo(Request $request, $id){
+    public function saveArchivo(Request $request){
         try{
             DB::beginTransaction();
 
-            $model = new relacion_inmueble_archivos();
-
-            if(!empty($id)){
-                $model = (new relacion_inmueble_archivos())->find($id);
+            if($request->tipo == 'imagen' && empty($request->images_file)){
+                return Response::statusJson("warning", 'Debe seleccionar al menos un archivo', "saveArchivo", true, true);
             }
 
-            dd($request->all());
-
-
-
-            $model =propietario::find($request->id);
-            $propietario = $model->saveData($request);
-
-            $agenda = null;
-            if(!empty($request->age_titulo) and !empty($request->age_descri) and !empty($request->age_fecha)){
-                $agenda = AgendaService::crearAgenda($request);
+            if($request->tipo == 'archivo'){
+                if(empty($request->documento_file)){
+                    return Response::statusJson("warning", 'Debe seleccionar un archivo', "saveArchivo", true, true);
+                }
+                if(empty($request->tipo_archivo)){
+                    return Response::statusJson("warning", 'Debe indicar el tipo de archivo', "saveArchivo", true, true);
+                }
             }
 
-            $request['agenda_id'] = (!empty($agenda)) ? $agenda->id : null;
-            $modulo = 'valoracon';
+            if($request->tipo == 'imagen'){
+                foreach ($request->images_file as $file) {
+                    $path = FilesService::uploadFile($request, $file);
+                    $model = new relacion_inmueble_archivos();
+                    $model->tipo = $request->tipo;
+                    $model->created_at = Carbon::now();
+                    $model->uuid = $request->uuid;
+                    $model->path = $path['path'];
+                    $model->name_file = $path['name'];
+                    $model->inmueble_id = (!empty($request->inmueble_id)) ? $request->inmueble_id : null;
+                    $model->save();
+                }
+            }
 
-            if($request->accion == 5) $modulo = 'baja';
-            if($request->accion == 6) $modulo = 'encargo';
-
-            $request['modulo'] = $modulo;
-
-            $valoracion = inmueble::find($request->id_inmueble);
-            $valoracion = $valoracion->saveValoracion($request);
+            if($request->tipo == 'archivo'){
+                $path = FilesService::uploadFile($request, $request->documento_file);
+                $model = new relacion_inmueble_archivos();
+                $model->tipo = $request->tipo;
+                $model->tipo_archivo = $request->tipo_archivo;
+                $model->created_at = Carbon::now();
+                $model->uuid = $request->uuid;
+                $model->path = $path['path'];
+                $model->name_file = $path['name'];
+                $model->inmueble_id = (!empty($request->inmueble_id)) ? $request->inmueble_id : null;
+                $model->save();
+            }
 
             DB::commit();
-            Response::status($request,"success",'Registro Actualizado Exitosamente!','saveValoracion', true);
-            return redirect()->back();
+
+            $archivos = (new relacion_inmueble_archivos())
+                ->selectRaw("relacion_inmueble_archivos.*, tipo_archivo.descripcion as tipoDescri")
+                ->where('uuid',$request->uuid)
+                ->leftjoin('tipo_archivo','tipo_archivo.id','=','relacion_inmueble_archivos.tipo_archivo')
+                ->get();
+            return Response::statusJson("success",'Archivo guardado Exitosamente!','saveArchivo',$archivos, true);
         }catch(\Exception $e){
             DB::rollback();
-            Response::status($request,"warning", $e->getMessage(), "saveValoracion", true, true);
-            return redirect()->back()->withInput($request->all());
+            return Response::statusJson("warning", $e->getMessage(), "saveArchivo", null, true, true);
         }
     }
-    public function deleteArchivo(Request $request, $id){
-        # code...
-    }
 
+    public function deleteArchivo(Request $request, $id){
+        try{
+            DB::beginTransaction();
+
+            $model = (new relacion_inmueble_archivos())->find($id);
+
+            if(!$model) return Response::statusJson("warning", 'El archivo no se encuentra registrado', "deleteArchivo", true, true);
+
+            $inmuebleId = $model->inmueble_id;
+            $uuid = $model->uuid;
+
+            FilesService::deleteFile($model->path);
+
+            $model->delete();
+            DB::commit();
+
+            $archivos = (new relacion_inmueble_archivos())
+                ->selectRaw("relacion_inmueble_archivos.*, tipo_archivo.descripcion as tipoDescri")
+                ->leftjoin('tipo_archivo','tipo_archivo.id','=','relacion_inmueble_archivos.tipo_archivo');
+            
+            if(!empty($inmuebleId)){
+                $archivos = $archivos->where('inmueble_id', $inmuebleId);
+            }else{
+                $archivos = $archivos->where('uuid', $uuid);
+            }
+
+            $archivos = $archivos->get();
+
+            return Response::statusJson("success",'Archivo guardado Exitosamente!','saveArchivo',$archivos, true);
+        }catch(\Exception $e){
+            DB::rollback();
+            return Response::statusJson("warning", $e->getMessage(), "saveArchivo", null, true, true);
+        }
+    }
 
     public function contrato($request){
 
